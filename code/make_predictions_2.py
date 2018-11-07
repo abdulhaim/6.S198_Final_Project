a1c3010a61a107bd77bfbb0f4415e03c249b1a9d
Marwa Abdulhai <marwaabdulhai@hotmail.com>
2018-11-07 00:53:07 -0500

    changes

--- code/make_predictions_2.py
+++ code/make_predictions_2.py
@@ -1,4 +1,4 @@
-import label_image 
+import cnn_output.label_image 
 
 def predict_on_frames():
     frame_predictions = []
@@ -16,12 +16,38 @@ def predict_on_frames():
 
     return frame_predictions
 
+def get_accuracy(predictions, labels):
+    """After predicting on each batch, check that batch's
+    accuracy to make sure things are good to go. This is
+    a simple accuracy metric, and so doesn't take confidence
+    into account, which would be a better metric to use to
+    compare changes in the model."""
+    correct = 0
+    for frame in predictions:
+        # Get the highest confidence class.
+        this_prediction = frame[0].tolist()
+        # print this_prediction
+        this_label = frame[1]
+        # print this_label
 
+        max_value = max(this_prediction)
+        max_index = this_prediction.index(max_value)
+        predicted_label = labels[max_index]
+        # print predicted_label
+
+        # Now see if it matches.
+        print predicted_label, this_label
+        if predicted_label.lower() == this_label.lower():
+            correct += 1
+        print correct, len(predictions)
+
+    print correct, len(predictions)
+    accuracy = correct / float(len(predictions))
+    return accuracy
 
 def main():
     with open('data/labeled-frames-' + batch + '.pkl', 'rb') as fin:
         frames = pickle.load(fin)
-
 
     predictions = predict_on_frames(frames, batch)
     for frame in predictions:
