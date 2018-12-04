ddddd4fa6bb76034fae8df7609bce50d13c5e29d
Marwa Abdulhai <marwaabdulhai@hotmail.com>
2018-12-04 18:57:49 -0500

    upload button working semi

--- demo/run_demo.py
+++ demo/run_demo.py
@@ -56,10 +56,53 @@ def determine_sign():
 
     downloads_dir = "/Users/Marwa/Downloads/"
 
-    predicted_word = run_pipeline(downloads_dir + "RecordedVideo.webm",direct_name ,model_file, label_file, input_height, input_width, input_mean, input_std, input_layer, output_layer, graph, input_name, output_name, input_operation, output_operation, sample_size)
+    predicted_word = run_pipeline(downloads_dir + file_name,direct_name ,model_file, label_file, input_height, input_width, input_mean, input_std, input_layer, output_layer, graph, input_name, output_name, input_operation, output_operation, sample_size)
 
-    if os.path.exists(downloads_dir + "RecordedVideo.webm"):
-        os.remove(downloads_dir + "RecordedVideo.webm")
+    if os.path.exists(downloads_dir + file_name):
+        os.remove(downloads_dir + file_name)
+    #remove segmented frames from directory in case you're 
+    #running multiple videos sequentially 
+
+    # shutil.rmtree(direct_name)
+    # os.mkdir(direct_name)
+
+    return json.dumps({"word":predicted_word})
+
+@app.route('/sample_video', methods=['GET'])
+def sample_determine_sign():
+    """Translate sign into word given signal to do so"""
+
+    file_name = "accept4.mov"
+    direct_name = "segmented_frames/"
+    model_file = "tf_files/retrained_graph.pb"
+    label_file = "tf_files/retrained_labels.txt"
+    # Uncomment 'model_file' and 'label_file' below to switch to 64 word model:
+    #model_file = "tf_files/retrained_graph2.pb"
+    #label_file = "tf_files/retrained_labels2.txt"
+
+    input_height = 224
+    input_width = 224
+    input_mean = 128
+    input_std = 128
+    input_layer = "input"
+    output_layer = "final_result"
+
+    input_name = "import/" + input_layer
+    output_name = "import/" + output_layer
+
+    graph = load_graph(model_file)
+    input_operation = graph.get_operation_by_name(input_name);
+    output_operation = graph.get_operation_by_name(output_name);
+
+    sample_size = 20 # number of images to randomly sample from the processed video
+
+    if not os.path.exists(direct_name):
+        os.makedirs(direct_name)
+
+    inner_dir = "/Users/Marwa/Documents/TestVideos/"
+
+    predicted_word = run_pipeline(inner_dir + file_name,direct_name ,model_file, label_file, input_height, input_width, input_mean, input_std, input_layer, output_layer, graph, input_name, output_name, input_operation, output_operation, sample_size)
+
     #remove segmented frames from directory in case you're 
     #running multiple videos sequentially 
 
